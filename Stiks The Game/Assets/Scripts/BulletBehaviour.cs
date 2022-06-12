d9bb66089cd706dac5a93d2b88acba30eb57ccc9
Cheenaswee <107208248+Cheenaswee@users.noreply.github.com>
2022-06-12 15:41:28 +0800

    added Script to Gunman in  Jason level

---
+++ Stiks The Game/Assets/Scripts/BulletBehaviour.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+
+{
+    GameObject target;
+    public float speed;
+    Rigidbody2D bulletRB;
+
+    private void Update()
+    {
+
+        bulletRB = GetComponent<Rigidbody2D>();
+        target = GameObject.FindGameObjectWithTag("Player");
+        Vector2 moveDir = (target.transform.position - transform.position).normalized * speed;
+        bulletRB.velocity = new Vector2(moveDir.x, moveDir.y);
+        Destroy(this.gameObject, 1);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Destroy(gameObject);
+    }
+
+}
