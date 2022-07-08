49115d1f4fee632ed39702099bfa48bcf85ee3d8
Cheenaswee <107208248+Cheenaswee@users.noreply.github.com>
2022-07-08 23:45:52 +0800

    added in animation for skill 1 and fixed the AI bug such that they will not collide with each other

--- Stiks The Game/Assets/Scripts/enemyAI/ImprovedPatrol.cs
+++ Stiks The Game/Assets/Scripts/enemyAI/ImprovedPatrol.cs
@@ -29,6 +29,13 @@ public class ImprovedPatrol : MonoBehaviour
         canShoot = true;
         
     }
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Enemies")
+        {
+            Physics2D.IgnoreCollision(collision.gameObject.GetComponent<Collider2D>(), GetComponent<Collider2D>());
+        }
+    }
 
     // Update is called once per frame
     void Update()
